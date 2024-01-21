6c502af1b9dc7aa85f6299d8bd908d0d9ba872af
deyvid <deyvidmitkov@gmail.com>
2024-01-20 20:14:46 -0500

    added missing adapter; backend ip

--- web-front/src/lib/shared.js
+++ web-front/src/lib/shared.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export function fetchDudeData(participants) {
     console.log("fetching data for", participants);
-    const url = "http://localhost/fetchDudeInfo";
+    const url = "http://34.228.255.157/fetchDudeInfo";
     return axios.post(url, {
             "clubID": "47454",
             "eventID": "13528",
