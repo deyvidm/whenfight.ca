f55ca6c4be9be481c5094ad214a7b5029184070a
deyvid <deyvidmitkov@gmail.com>
2024-07-08 21:21:51 -0400

    static -> node adapter

--- web-front/svelte.config.js
+++ web-front/svelte.config.js
@@ -1,5 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
-import sveltePreprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-node';
 import preprocess from "svelte-preprocess";
 
 
@@ -13,7 +12,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
 	}
 };
 
